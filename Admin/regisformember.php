77769ac45647250a5da2a2d1db8f284e8b28b891
THANAWUT\Ready Go!! <thanawut.dth@gmail.com>
2017-07-30 18:17:25 +0700

    add meta_admin

--- Admin/regisformember.php
+++ Admin/regisformember.php
@@ -1,44 +1,12 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>ComputerService</title>
-<link href="../css/login.css" rel="stylesheet" type="text/css" />
-<link href="../css/registextbox.css" rel="stylesheet" type="text/css" />
-<style type="text/css">
-body { 
-	background-color: #FFFFFF;
-	background-image: url(Image/images
-%20(1).jpg);
-	background-image: url(../Image/white-wallpaper-18.jpg);
-}
-</style>
-</head>
+<? include('meta_admin.php');?>
 
-<body topmargin="0">
-<table width="200" border="0" align="center">
-  <tbody>
     <tr>
-      <th colspan="2" scope="col"><img src="../Image/header2.jpg" width="911" height="212" alt=""/></th>
-    </tr>
-    <tr>
-      <td width="248" height="72">
-        <div id="login">
+      <td width="248">
   
- 
-    <a href="/project2/Admin/adminlogin.php"><h1>Computerservice</h1></a> 
+<? include("sidebar_admin.php");?>
+
 
  
-<a href="/project2/Admin/adminlogin.php"> <input  type="button" name="button1" id="button1" value="เพิ่มแอด+ช่าง"  ></a>
- <a href="/project2/Admin/Admin_Technic.php"> <input  type="button" name="button1" id="button1" value="รายชื่อแอด+ช่าง"  ></a>
-  <a href="/project2/Admin/memberlist.php"><input type="button" name="button5" id="button5" value="รายการสมาชิก"></a>
-  <a href="/project2/Admin/list_repair.php"><input type="button" name="button6" id="button6" value="รายการแจ้งซ่อม"></a>
-  <a href="/project2/Admin/Todevice.php"><input type="button" name="button7" id="button7" value="รายการเบิก-คืน"></a>
-  <a href="/project2/Admin/Webboard.php"><input type="button" name="button8" id="button8" value="กระทู้"></a>
-  <a href="/project2/Admin/regisformember.php"><input type="button" name="button9" id="button9" value="สมัครสมาชิก"></a>
-  <a href="/project2/Admin/report.php"><input type="button" name="button2" id="button3" value="รายงาน"></a>
-  <a href="/project2/Admin/Evalution.php"><input type="button" name="button2" id="button4" value="ประเมิน"></a>
-        </div>
 
       </td>
         
---
+++ Admin/sidebar_admin.php
@@ -0,0 +1,19 @@
+<div id="login">
+ 
+
+
+  <a href="<?=site_url()?>Admin/adminlogin.php"><h1>Computerservice</h1></a> 
+
+ 
+ <a href="<?=site_url()?>Admin/adminlogin.php"> <input  type="button" name="button1" id="button1" value="เพิ่มแอด+ช่าง"  ></a>
+ <a href="<?=site_url()?>Admin/Admin_Technic.php"> <input  type="button" name="button1" id="button1" value="รายชื่อแอด+ช่าง"  ></a>
+  <a href="<?=site_url()?>Admin/memberlist.php"><input type="button" name="button5" id="button5" value="รายการสมาชิก"></a>
+  <a href="<?=site_url()?>Admin/list_repair.php"><input type="button" name="button6" id="button6" value="รายการแจ้งซ่อม"></a>
+  <a href="<?=site_url()?>Admin/Todevice.php"><input type="button" name="button7" id="button7" value="รายการเบิก-คืน"></a>
+  <a href="<?=site_url()?>Admin/Webboard.php"><input type="button" name="button8" id="button8" value="กระทู้"></a>
+  <a href="<?=site_url()?>Admin/regisformember.php"><input type="button" name="button9" id="button9" value="สมัครสมาชิก"></a>
+  <a href="<?=site_url()?>Admin/report.php"><input type="button" name="button2" id="button3" value="รายงาน"></a>
+  <a href="<?=site_url()?>Admin/Evalution.php"><input type="button" name="button2" id="button4" value="ประเมิน"></a>
+        </div>
+        
+      </div>
