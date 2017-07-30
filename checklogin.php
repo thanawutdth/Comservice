5a39766456017fc1e08f984ed3aa6dbdb5c22990
sereepap <sereepap2029@gmail.com>
2017-07-30 17:19:58 +0700

    stage 2

--- checklogin.php
+++ checklogin.php
@@ -2,20 +2,21 @@
 include('meta.php');
 require_once("model/m_user.php");
 $m_user = new M_user;
-$user_dat=$m_user->login_user($_POST['username'],$_POST['password']);
+$user_dat=$m_user->login_user($_POST['txtUsername'],$_POST['txtPassword']);
+print_r($_POST);
 if (isset($user_dat['username'])) {
 	$_SESSION['username']=$user_dat['username'];
 	$_SESSION['position']=$user_dat['position'];
-	if($objResult["position"] == 1)// 1 = Admin
+	if($user_dat["position"] == 1)// 1 = Admin
 	   	    {
 			    echo "<meta http-equiv='refresh' content='0;url=Admin/adminlogin.php'>";
 			}
-		   if($objResult["position"] == 2)// 2 = Technic
+		   if($user_dat["position"] == 2)// 2 = Technic
 		   	{
 				echo "<meta http-equiv='refresh' content='0;url=Technic/techniclogin'>";
 				//header("location:staff/index.php");
 			}
-			if($objResult["position"] == 3)// 2 = member
+			if($user_dat["position"] == 3)// 2 = member
 		   	{
 				echo "<meta http-equiv='refresh' content='0;url=Member/firstpage_member.php'>";
 				//header("location:staff/index.php");
@@ -24,7 +25,7 @@ if (isset($user_dat['username'])) {
 	?>
         <script type="text/javascript">
         		alert('username and password incorrect');
-        		window.open("<?echo site_url('admin/logout.php');?>","_self");	        	
+        		//window.open("<?echo site_url('logout.php');?>","_self");	        	
         </script>
 		<?
 }
--- meta.php
+++ meta.php
@@ -12,6 +12,7 @@ include('helper_func.php');
 <title>ComputerService</title>
 <link href="css/login.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
+
 body {
 	background-color: #FFFFFF;
 	background-image: url(Image/images
@@ -19,7 +20,14 @@ body {
 	background-image: url(Image/white-wallpaper-18.jpg);
 }
 </style>
-
+<!-- <link href="css/bootstrap.css" rel="stylesheet" type="text/css"> -->
+<link href="../css/bootstrap-3.3.4.css" rel="stylesheet" type="text/css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
 
 <body topmargin="0" >
