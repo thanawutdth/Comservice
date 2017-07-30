7dde363c34c7f2047ec460e2e90592ea0cf60a94
sereepap <sereepap2029@gmail.com>
2017-07-30 16:20:47 +0700

    change connect

--- model/db.php
+++ model/db.php
@@ -1,8 +1,8 @@
 <?php 
 
-	$dbuser = 'root';
-	$dbpass = 'root';
-	$dbh = new PDO('mysql:host=localhost;dbname=nml2016', $dbuser, $dbpass);
+	$dbuser = 'comservice';
+	$dbpass = '1234';
+	$dbh = new PDO('mysql:host=localhost;dbname=project2', $dbuser, $dbpass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
