db2969b8c89dc93bcbb139aec75de9300be6e8cf
THANAWUT\Ready Go!! <thanawut.dth@gmail.com>
2017-07-31 01:42:07 +0700

    edit sidebar

---
+++ Technic/Todevice.php
@@ -0,0 +1,34 @@
+<? include('meta_technic.php');?>
+
+    <tr valign="top">
+      <td width="248">
+	  <? include("sidebar_technic.php");?>
+      </td>
+
+  
+        
+      <td width="657" rowspan="9" align="center" valign="top">&nbsp;&nbsp;<table width="518" height="52" border="1" align="center">
+        <tbody>
+          <tr>
+            <th width="87" height="21" scope="col">name</th>
+            <th width="93" scope="col">lastname</th>
+            <th width="57" scope="col">type</th>
+            <th width="40" scope="col">date</th>
+            <th width="102" align="center" scope="col">deleate</th>
+            </tr>
+   
+        </tbody>
+      </table></td>
+    </tr>
+    
+    
+    
+    <tr>
+      <td height="19" colspan="2">&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
--- Technic/sidebar_technic.php
+++ Technic/sidebar_technic.php
@@ -9,6 +9,7 @@
  <a href="<?=site_url()?>Technic/list_technician.php"> <input  type="button" name="button1" id="button1" value="รายชื่อช่างซ่อม"  ></a>
   
   <a href="<?=site_url()?>Technic/list_repair.php"><input type="button" name="button6" id="button6" value="รายการแจ้งซ่อม"></a>
+   <a href="<?=site_url()?>Technic/pickupDevice.php"><input type="button" name="button7" id="button7" value="เบิกอุปกรณ์"></a>
   <a href="<?=site_url()?>Technic/pickupDevice.php"><input type="button" name="button7" id="button7" value="รายการเบิก-คืน"></a>
   <a href="<?=site_url()?>Technic/Webboard.php"><input type="button" name="button8" id="button8" value="กระทู้"></a>
   <a href="<?=site_url()?>Technic/report.php"><input type="button" name="button2" id="button3" value="รายงาน"></a>
