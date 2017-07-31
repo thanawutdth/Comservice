aa4429f2c58c63f2e9dd18c0dcef98349acb21c9
THANAWUT\Ready Go!! <thanawut.dth@gmail.com>
2017-07-31 18:30:26 +0700

    delete buttom listtech

--- Technic/sidebar_technic.php
+++ Technic/sidebar_technic.php
@@ -6,7 +6,7 @@
 
  
   <a href="<?=site_url()?>Technic/editdatatech.php"> <input  type="button" name="button1" id="button1" value="แก้ไขข้อมูลส่วนตัว"  ></a>
- <a href="<?=site_url()?>Technic/list_technician.php"> <input  type="button" name="button1" id="button1" value="รายชื่อช่างซ่อม"  ></a>
+ 
   
   <a href="<?=site_url()?>Technic/list_repair.php"><input type="button" name="button6" id="button6" value="รายการแจ้งซ่อม"></a>
    <a href="<?=site_url()?>Technic/Todevice.php"><input type="button" name="button7" id="button7" value="รายการอุปกรณ์"></a>
